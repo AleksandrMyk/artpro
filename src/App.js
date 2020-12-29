2ee41b382003aa88782b0cce6409c0ba36e60ed8
Aleksandr Mykhailov <ss.mykhailov@gmail.com>
2020-12-30 01:06:53 +0200

    ''

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>HI!!</h1>
+      </>
+    );
+  }
+}
+export default App;
